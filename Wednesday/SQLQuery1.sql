33f3d3c8dff783e69a1927aecc7c9ac384826904
alexandru.erbasu <erbasualex@gmail.com>
2017-07-12 17:57:02 +0300

    Wednesday commit

---
+++ Wednesday/SQLQuery1.sql
@@ -0,0 +1,135 @@
+﻿--DROP DATABASE	MyDatabase
+
+--create table Persoana(
+--ID int,
+--Nume nvarchar(200),
+--Prenume nvarchar(200)
+--)
+
+--alter table Persoana
+--add Varsta tinyint
+
+--alter table Persoana
+--drop column Varsta
+
+--alter table Persoana
+--alter column Varsta int
+
+--insert into Persoana(ID, Nume, Prenume)
+--values (1,N'Ştefănescu',N'Robert')
+
+--select ID, Nume, Prenume from Persoana 
+
+
+--update Persoana
+--set Varsta = 27,
+--Nume = 'Stefanescu'
+--where ID = 1
+
+--delete from Persoana 
+--where ID = 1
+
+--insert into Persoana(Id, Nume, Prenume, Varsta)
+--values (1, N'Stefanescu', N'Robert', 27)
+--       , (2, N'Popescu', N'Popica', 50)
+--       , (3, N'Marinescu', N'Marinica', 14)
+--       , (4, N'Ionescu', N'Ionica', 20)
+--       , (5, N'Georgescu', N'Georgica', 34)
+
+--select * 
+--from Persoana
+----where varsta between 10 and 30
+--where Prenume like '[I-R]%'
+
+--select *
+--from Persoana
+--order by Nume desc, Prenume asc 
+
+--select MIN(varsta) --MAX
+--from Persoana
+
+--select COUNT(*)
+--from Persoana
+
+--select len(nume), * --lower(nume),upper(nume)
+--from Persoana
+
+--alter table Persoana
+--add NumePrenume as Nume + ' ' + Prenume
+
+--alter table Persoana
+--drop column Varsta
+
+--alter table Persoana
+--add DataNasterii date
+
+--alter table Persoana
+--add Varsta as datediff(yy,DataNasterii,getDate())
+
+--select *
+--from Persoana
+
+--insert into Oras(ID, Nume, Tara, Populatie)
+--values (1,'Constanta','Ungaria',500000),
+--(2,'Londra','UK',8000000),
+--(3,'Berlin','Germania',600000)
+
+--alter Table persoana
+--Add orasID int
+
+--select * 
+--from Oras
+
+--select * 
+--from Persoana
+
+--Select *
+--from persoana
+--inner join Oras On persoana.orasID = Oras.ID
+
+--select p.Id, p.NumePrenume, o.Nume as NumeOras
+--from Persoana as p
+--inner join Oras as o on p.orasID = o.ID
+
+--Exercitiu:
+
+--alter table Oras
+--drop column tara
+
+--create table Tara(id int,
+--				Nume varchar(200)) 
+
+--alter table Oras
+--add TaraId int
+
+--insert into Tara (id,nume)
+--values (1,'UK'),
+--	(2,'Romania'),
+--	(3,'Germania'),
+--	(4,'Olanda')
+
+--select Persoana.ID, Persoana.Nume, Oras.Nume, Tara.Nume
+--from Persoana
+--inner join Oras on Persoana.OrasId = Oras.ID
+--inner join Tara on Oras.ID = Tara.id
+
+--Persoanele care au Varsta mai mare decat media de varsta din tara din care fac parte
+select
+       p.Id
+       , P.NumePrenume
+       , p.Varsta
+       , t.Nume
+       , medieDeVarsta.MedieVarsta
+from Persoana p
+inner join oras o on p.OrasId = o.Id
+inner join tara t on o.TaraId = t.Id
+inner join (
+             select
+                    t1.Id as IdTara
+                    , t1.Nume
+                    , avg(p1.Varsta) as MedieVarsta
+             from persoana p1
+                    inner join oras o1 on p1.OrasId = o1.Id
+                    inner join tara t1 on o1.TaraId = t1.Id
+             group by t1.Id, t1.Nume) medieDeVarsta on t.Id = medieDeVarsta.IdTara
+where p.Varsta > medieDeVarsta.MedieVarsta 
---
+++ Wednesday/SQLQuery2 (table melodie).sql
@@ -0,0 +1,78 @@
+--create table Melodie(
+--ID int,
+--Titlu nvarchar(100),
+--Artist nvarchar(200),
+--DataLansare datetime,
+--RankMaxim int
+--)
+--Insert into Melodie (ID, titlu, Artist, DataLansare, RankMaxim)
+--values (1,'Despacito','Daddy Yankee','2017-01-28',1),
+--		(2,'Story of OJ','Jay-Z','2017-07-2',7),
+--		(3,'Rap God','Eminem','2016-03-31',1);
+
+--select *
+--from Melodie
+--order by RankMaxim
+
+--drop table melodie
+
+--create table Melodie(
+--ID int,
+--Nume nvarchar(100),
+--AlbumId int,
+--Durata int
+--)
+
+--create table Album(
+--ID int,
+--Nume nvarchar(100),
+--ArtistId int,
+--Gen nvarchar(100),
+--DataLansarii DateTime
+--)
+
+--create table Artist(
+--ID int,
+--Nume nvarchar(100)
+--)
+
+--alter table melodie drop column durata
+
+--alter table melodie add durata time
+
+
+--Insert into Melodie (ID, nume, AlbumId, Durata)
+--values (1,'Melodie1',1,'00:03:33'),
+--		(2,'Melodie2',1,'00:02:13'),
+--		(3,'Melodie3',2,'00:06:13'),
+--		(4,'Melodie4',2,'00:02:53'),
+
+--		(5,'Melodie5',3,'00:01:53'),
+--		(6,'Melodie6',3,'00:04:43'),
+--		(7,'Melodie7',4,'00:03:13'),
+--		(8,'Melodie8',4,'00:03:53')
+
+--insert into Album (id,nume,artistID,gen,datalansarii)
+--values (1,'Album1',1,'Reagge','1997-01-28'),
+-- (2,'Album2',1,'Reagge','2000-01-28'),
+-- (3,'Album3',2,'Rap','2008-04-28'),
+--(4,'Album4',2,'Rap','2017-06-28')
+
+--insert into Artist(id, nume)
+--values (1,'Artist1'),
+--(2,'Artist2')
+
+select * from Melodie
+
+select * from Album
+
+select * from Artist
+--insert into Melodie 
+--values (9,'Melodie9',1,'00:04:33')
+
+
+select ar.Nume as NumeArtist, al.Nume as NumeAlbum, count(m.AlbumId) as NoOfSongs--, AVG(m.durata)
+from Melodie as m
+inner join album as al on m.AlbumId = al.ID
+inner join artist as ar on al.ArtistId = ar.ID
+group by ar.Nume, al.Nume
